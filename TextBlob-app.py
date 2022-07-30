27fbc3b4fed45c1b2f868a36f428d31713208216
KaoHu <kaohulogist@gmail.com>
2022-07-30 03:25:27 -0500

    req style jpg

--- TextBlob-app.py
+++ TextBlob-app.py
@@ -1,7 +1,24 @@
 import streamlit as st
 from textblob import TextBlob
+from PIL import Image
+
+
+with open("style.css") as f:
+    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+col1, col2,col3 = st.columns([1,1,1])
+col2.image(Image.open('KH.jpg'))
 
 st.markdown('''
-# Web Application for text-processing with Natural Language Processing
+## Natural Language Processing Web Application
+- **Python libraries:** streamlit, textblob, PIL, playsound
+
 ''')
+
+user_words_input = TextBlob(st.sidebar.text_input('Please enter the word(s): '))
+
+st.sidebar.multiselect('NLPK features:', ['tags', 'noun_phrases', 'sentiment', 'definitions','count','correct','spellcheck'])
+
+
 
