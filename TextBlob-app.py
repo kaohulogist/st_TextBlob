b8672e80b5dae69625cef97b598c854922b02a43
KaoHu <kaohulogist@gmail.com>
2022-07-30 04:58:35 -0500

    update req

--- TextBlob-app.py
+++ TextBlob-app.py
@@ -1,7 +1,12 @@
+from re import A
+from typing import Text
 import streamlit as st
 from textblob import TextBlob
 from PIL import Image
-
+from newspaper import Article
+from textblob import Word
+import nltk
+nltk.download('omw-1.4')
 
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
@@ -16,9 +21,20 @@ st.markdown('''
 
 ''')
 
-user_words_input = TextBlob(st.sidebar.text_input('Please enter the word(s): '))
+user_words_input = st.sidebar.text_input('Please enter the word(s): ')
+blob_user_words_input = TextBlob(user_words_input)
+TextBlob_functions = ['word_counts','tags', 'noun_phrases', 'sentiment', 'definitions','count','correct','spellcheck']
 
-st.sidebar.multiselect('NLPK features:', ['tags', 'noun_phrases', 'sentiment', 'definitions','count','correct','spellcheck'])
-
-
+if st.sidebar.button('tags'):
+    st.write(blob_user_words_input.tags)
+if st.sidebar.button('word counts'):
+    st.write(blob_user_words_input.word_counts)
+if st.sidebar.button('noun phrases'):
+    st.write(blob_user_words_input.noun_phrases)
+if st.sidebar.button('definitions'):
+    for word in blob_user_words_input.words:
+        w = Word(word.lower())
+        st.write(w.definitions)
+    
+user_url_input = st.sidebar.text_input('Please enter the URL')
 
